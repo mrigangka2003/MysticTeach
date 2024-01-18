a785926d7b8957642df37a74141674d77a2970d8
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-01-19 00:04:30 +0530

    a small changes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const isLoggedIn = require('../middleware/isLoggedIn.js') ;
 
 router.get("/dashboard", isLoggedIn, (req, res) => {
-  const message = '<center> <h1>authenticated successfully whats next ?</h1></center>'
-  res.send(message);
+ res.render('dashboard') ;
 });
 
 module.exports = router;
